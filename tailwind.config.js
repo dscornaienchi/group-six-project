26de6dd47ec269a8101e6dbc7a797c63de9af507
TheGrimsNFT <thegrimsnftcollection@gmail.com>
2023-09-19 21:43:28 -0500

    subtle css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '2xl': '0 2px 10px rgba(0, 0, 0, 0.2), 0 6px 20px rgba(0, 0, 0, 0.19)',
+    
+      },
+    },
   },
   plugins: [],
-}
+};
 
